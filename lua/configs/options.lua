66309961e8bbaffff510df298a7358f3cc2fa66c
Ivan Lytvynenko <lytvynenko.i.d@gmail.com>
2026-06-11 20:30:30 +0200

    fix: remove deprecated method

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -91,7 +91,7 @@ vim.cmd [[autocmd BufWritePre * %s/\s\+$//e]]
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
     group = highlight_group,
     pattern = '*',
