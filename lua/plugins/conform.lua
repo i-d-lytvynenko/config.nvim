f6141a2101ed738ef00136ba1f5231400cde620f
Ivan Lytvynenko <lytvynenko.i.d@gmail.com>
2026-06-21 18:24:20 +0200

    feat: guix formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,12 +2,24 @@
 local config = function()
     local conform = require 'conform'
 
+    local formatters = {}
+    local formatters_by_ft = {
+        lua = { 'stylua' },
+        go = { 'gofumpt' },
+        javascript = { 'prettierd', 'prettier' },
+    }
+
+    if vim.fn.isdirectory('/gnu/store') == 1 then
+        formatters_by_ft.scheme = { 'guix_style' }
+        formatters.guix_style = {
+            command = 'guix',
+            args = { 'style', '-f', '$FILENAME' },
+            stdin = false,
+        }
+    end
     conform.setup {
-        formatters_by_ft = {
-            lua = { 'stylua' },
-            go = { 'gofumpt' },
-            javascript = { 'prettierd', 'prettier' },
-        },
+        formatters_by_ft = formatters_by_ft,
+        formatters = formatters,
     }
     vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
         conform.format {
